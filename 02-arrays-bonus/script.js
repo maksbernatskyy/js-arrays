704f8f74a833d15f6ff933b02615a26d4ea5b3cd
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-08 16:24:43 +0200

    complete task 3 of bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -25,6 +25,8 @@ for (i = 0; i < teachers.length; i++){
 console.log(longNames)
 
 // 3. Rimuovi 'Ed' dall'array teachers
+teachers.splice(5, 1)
+console.log(teachers)
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
